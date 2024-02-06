c2125065c2c23f83e6e95d7a52a610f133086b1e
Iskander-rzk <sasha1koval@gmail.com>
2024-02-06 16:29:44 +0700

    alg_lab_1 1

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/C++_study
+# Build directory: C:/C++_study/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
