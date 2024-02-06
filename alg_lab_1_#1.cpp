5b53383db8e1d856aaf76a4fb6dbac5233904b47
Iskander-rzk <sasha1koval@gmail.com>
2024-02-06 17:32:16 +0700

    lol

--- alg_lab_1_#1.cpp
+++ alg_lab_1_#1.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
+#include <list>
+#include <random>
+#include <cstdlib>
 
 int main()
 {
-    int n1, n2, n3;
-    std::cin >> n1 >> n2 >> n3;
-    if (n1 == n2 && n1 == n3) std::cout << 3;
-    else if ( n1 != n2 && n1 != n3 && n2 != n3) std::cout << 0;
-    else std::cout << 2;
+    int n = 0;
+    int m = 0;
+    int max_sum;
+    int lst1[25] = { 27, 83, 14, 62, 75, 41, 29, 6, 97, 50, 18, 67, 9, 32, 89, 55, 23, 79, 38, 91, 12, 58, 21, 72, 43 };
+    for (int i = 0; i < 25; i++)
+    {
+        n = n + lst1[i];
+        if (m < lst1[i])
+        {
+            m = lst1[i];
+            max_sum = n;
+        }
+    }
+    std::cout << max_sum << std::endl;
+    std::cout << n - max_sum;
     return 0;
 }
