6c1f249baad0f48896a34a0153ccf97fdc716052
Iskander-rzk <sasha1koval@gmail.com>
2024-02-19 20:30:53 +0700

    /

--- main.cpp
+++ main.cpp
@@ -2,39 +2,62 @@
 #include <list>
 #include <random>
 #include <cstdlib>
+#include <fstream>
 #include <ctime>
-
-int main(int mylist)
+  
+void add(int* R);
+void subtract(int, int);
+void multiply(int, int);
+  
+int main()
 {
+    int N = 10000;
+    int* R = new int[N];
+    std::random_device rd; // Используем генератор случайных чисел
+
+    std::mt19937 gen(rd()); // Создаем генератор случайных чисел на основе seed
+    std::uniform_int_distribution<int> dist(0, 10000); // Определение диапазона случайных чисел
+
+    for (int i = 0; i < N; i++) // Вывод исходного массива 
+    {
+        R[i] = dist(gen);
+    }   
+    void (*operations[1])(int, int) = {add};
+       
+    // получаем длину массива
+    unsigned length = std::size(operations);
+       
+    for(unsigned i{}; i < length; i++)
+    {
+        operations[i](R);    // вызов функции по указателю
+    }
+}
+void add(int* R)
+{
+    std::ofstream myfile;
+    myfile.open("example.csv");
+    myfile << "X;Y2;Y3\n"; // запись заголовка из двух ячеек
+    int y2, y3;
+
+    int k = 0;
+    unsigned int start_time = clock();
     int n = 0;
     int m = 0;
     int max_sum;
-    int lst1[25] = { 27, 83, 14, 62, 75, 41, 29, 6, 97, 50, 18, 67, 9, 32, 89, 55, 23, 79, 38, 91, 12, 58, 21, 72, 43 };
-    for (int k : mylist)
+    k += 3;
+
+    for (int i = 0; i < 25; i++)
     {
-        n = n + k;
-        if (m < k)
+        n = n + R[i];
+        if (m < R[i])
         {
-            m = k;
+            m = R[i];
             max_sum = n;
         }
+        k += 6;
     }
-
     std::cout << max_sum << std::endl;
-    std::cout << n - max_sum;
-    return 0;
-}
-
-int random() {
-    std::srand(std::time(nullptr)); // use current time as seed for random generator
-    int random_value = std::rand();
-    std::list<int> mylist;
-
-    for (int n = 0; n != 25; n++)
-    {
-        int x = std::rand(); // Note: 1 + rand() % 6 is biased
-        mylist.push_back(x);
-    }
-    int a = 0;
-    return mylist;
+    std::cout << n - max_sum << std::endl;
+    unsigned int end_time = clock(); // конечное врем
+    std::cout << end_time << std::endl;
 }
