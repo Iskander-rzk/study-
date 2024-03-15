93330c3f24340edead070c048544ca85ab6bce85
Iskander-rzk <sasha1koval@gmail.com>
2024-03-16 05:22:13 +0700

    .

--- main.cpp
+++ main.cpp
@@ -1,24 +1,72 @@
 #include <iostream>
 #include <string>
-#include <bitset>
+using namespace std;
 
-int main()
-{
-	int n = 0, b = 0, c, N = 0, l = 0;
-    signed short a;
-	std::cin >> a;
-	int i = 0;
-	while (i < a.size())
-	{
-		if (a[i] == '0')
-		{
-			if (n > N) N = n;
-			n = 0;
-			b = 1;
-		}
-		else if (a[i] == '1' && b == 1) n++;
-		i++;
-	}
-	std::cout << N;
-	return 0;
+//По значению:
+
+string convertToBase(int num, int base) {
+    string res = "";
+    while (num > 0) {
+        int rem = num % base;
+        res = to_string(rem) + res;
+        num /= base;
+    }
+    return res;
 }
+
+int main() {
+    int num, base;
+    cout << "Enter the decimal number: ";
+    cin >> num;
+    cout << "Enter the base of the new number system: ";
+    cin >> base;
+    cout << "The number " << num << " in base " << base << " is " << convertToBase(num, base) << endl;
+    return 0;
+}
+
+
+//По ссылке:
+/*/
+void convertToBase(int num, int base, string& res) {
+    while (num > 0) {
+        int rem = num % base;
+        res = to_string(rem) + res;
+        num /= base;
+    }
+}
+
+int main() {
+    int num, base;
+    string result = "";
+    cout << "Enter the decimal number: ";
+    cin >> num;
+    cout << "Enter the base of the new number system: ";
+    cin >> base;
+    convertToBase(num, base, result);
+    cout << "The number " << num << " in base " << base << " is " << result << endl;
+    return 0;
+}
+
+
+//Через указатель:
+
+void convertToBase(int num, int base, string* res) {
+    while (num > 0) {
+        int rem = num % base;
+        *res = to_string(rem) + *res;
+        num /= base;
+    }
+}
+
+int main() {
+    int num, base;
+    string result = "";
+    cout << "Enter the decimal number: ";
+    cin >> num;
+    cout << "Enter the base of the new number system: ";
+    cin >> base;
+    convertToBase(num, base, &result);
+    cout << "The number " << num << " in base " << base << " is " << result << endl;
+    return 0;
+}
+/*/
