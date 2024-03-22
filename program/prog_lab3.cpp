41249ca1e1e48c0aebd28977da806c484ff4158b
Iskander-rzk <sasha1koval@gmail.com>
2024-03-23 05:45:11 +0700

    .

--- program/prog_lab3.cpp
+++ program/prog_lab3.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main() {
     const int N = 10;  
-	int massive[N];
-	//заполняем элементы матрицы с клавиатуры построчно
-	int i, j;
-	for (i = 0; i < N; i++)
+    int massive[N];
+    std::cout << "Enter array: ";
+	for (int i = 0; i < N; i++)
     {
         std::cin >> massive[i];
     }
 
     int flag = 0, pause = 0;
-    int* new_massive = new int[];
-    for (i = 0; i < N; i++)
+    std::vector<int> new_massive;
+    for (int i = 0; i < N; i++)
     {
-        if (massive[i] != 0) new_massive[i-pause] = massive[i], flag++;
-        else if (flag == 0 or flag == 2 ) new_massive[i-pause] = massive[i];
-        else pause++;
+        if (massive[i] != 0) new_massive.push_back(massive[i]), flag++;
+        else if (flag == 0 || flag == 2 ) new_massive.push_back(massive[i]);
     }
 	
     
     //Вывод всех элементов матрицы на экран построчно
-    for (int a = 0; a < N; a++) std::cout << new_massive[a] << "  ";
+    std::cout << "New array: ";
+    for (int a = 0; a < new_massive.size(); a++) std::cout << new_massive[a] << "  ";
 	return 0;
 }
